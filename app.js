a340a72ac8a5808ad01289694eb7501d302aaa48
NguyenCaoThin <nguyenthin23102000@gmail.com>
2021-03-04 08:31:27 +0700

    Create a frame for the interface

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Render song
+ * 2. Scroll top
+ * 3. Play / pause / seek
+ * 4. CD rotate
+ * 5. Next / prev
+ * 6. Random
+ * 7. Next / Repeat when ended
+ * 8. Active song 
+ * 9. Scroll active song into view
+ * 10. Play son when click
+ */
